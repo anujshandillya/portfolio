c4c8dfdd82a9ddfa0ed9fafb46b3c8a6876cc104
Anuj Sharma <anujsharmawork11@gmail.com>
2023-12-15 01:19:32 +0530

    lag fixes+orbit controls

--- src/components/canvas/Stars.jsx
+++ src/components/canvas/Stars.jsx
@@ -1,6 +1,6 @@
 import { useState, useRef, Suspense } from "react";
 import { Canvas, useFrame } from "@react-three/fiber";
-import { Points, PointMaterial, Preload } from "@react-three/drei";
+import { Points, PointMaterial, Preload, OrbitControls } from "@react-three/drei";
 import * as random from "maath/random/dist/maath-random.esm";
 
 const Stars = (props) => {
@@ -36,9 +36,9 @@ const StarsCanvas = () => {
     <div className="w-full h-auto absolute inset-0 z-[-1]">
       <Canvas camera={{ position: [0, 0, 1] }}>
         <Suspense fallback={null}>
+          <OrbitControls enableZoom={false} />
           <Stars />
         </Suspense>
-
         <Preload all />
       </Canvas>
     </div>
