8cb50fbe92a9d3d0a2019eff3d07973f812bcc60
Anuj Sharma <anujsharmawork11@gmail.com>
2024-01-14 22:33:56 +0530

    env

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -44,10 +44,10 @@ const Contact = () => {
       }
       emailjs
         .sendForm(
-          import.meta.env.REACT_APP_SERVICE_ID,
-          import.meta.env.REACT_APP_TEMPLATE_ID,
+          import.meta.env.VITE_SERVICE_ID,
+          import.meta.env.VITE_TEMPLATE_ID,
           form.current,
-          import.meta.env.REACT_APP_PUBLIC_KEY
+          import.meta.env.VITE_PUBLIC_KEY
         )
         .then((result) => {
           console.log(result.text);
