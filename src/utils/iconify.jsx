0b0b2f864574d4ba0e538543d6c950bdece08d5d
anujshandillya <anujsharmawork11@gmail.com>
2025-07-20 12:43:23 +0530

    add: iconify component

---
+++ src/utils/iconify.jsx
@@ -0,0 +1,27 @@
+import { forwardRef } from "react";
+import { Icon } from "@iconify/react";
+import PropTypes from "prop-types";
+import { useSelector } from "react-redux";
+
+const Iconify = forwardRef(
+  ({ icon, width = 20, color, className }, ref) => {
+    const theme=useSelector((state)=>state.mode);
+  const bool=theme==='dark';
+    return (
+      <div ref={ref} className={className}>
+        <Icon icon={icon} width={width} height={width} color={bool ? "white" : "black"} />
+      </div>
+    );
+  }
+);
+
+Iconify.propTypes = {
+  icon: PropTypes.string.isRequired,
+  width: PropTypes.number,
+  color: PropTypes.string,
+  className: PropTypes.string,
+};
+
+Iconify.displayName = "Iconify";
+
+export default Iconify;
